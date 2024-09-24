8fb7ccaa80ca5202a93394b2584c47e5137d2696
JMPolvillo <rothkildam@gmail.com>
2024-09-24 10:18:42 +0200

    updated Flight model

--- src/main/java/com/flightbookings/flight_bookings/models/Flight.java
+++ src/main/java/com/flightbookings/flight_bookings/models/Flight.java
@@ -1,4 +1,38 @@
 package com.flightbookings.flight_bookings.models;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Task")
+
 public class Flight {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Flight(Long id) {
+        this.id = id;
+    }
+
+    @OneToMany(mappedBy = "flight")
+    @JsonManagedReference
+    private List<Seat> seatList;
+
+    @OneToMany(mappedBy = "flight")
+    @JsonManagedReference
+    private List<Booking> bookingList;
+
 }
