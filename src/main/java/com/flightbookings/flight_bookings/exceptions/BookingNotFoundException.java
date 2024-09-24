3358b3bc66ed6b48ba1e09f24bd51a4d57ebb9a0
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-24 13:55:10 +0200

    feat: add exception for booking not found

--- src/main/java/com/flightbookings/flight_bookings/exceptions/BookingNotFoundException.java
+++ src/main/java/com/flightbookings/flight_bookings/exceptions/BookingNotFoundException.java
@@ -1,2 +1,12 @@
-package com.flightbookings.flight_bookings.exceptions;public class BookingNotFoundException {
+package com.flightbookings.flight_bookings.exceptions;
+
+public class BookingNotFoundException extends RuntimeException {
+
+    public BookingNotFoundException(String message) {
+        super(message);
+    }
+
+    public BookingNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
