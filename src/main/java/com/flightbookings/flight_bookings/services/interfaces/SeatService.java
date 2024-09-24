82a41941e9cab381ca1af24dccebe85522a7aef9
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-24 11:56:35 +0200

    feat: add method overrided in seatServiceImpl

--- src/main/java/com/flightbookings/flight_bookings/services/interfaces/SeatService.java
+++ src/main/java/com/flightbookings/flight_bookings/services/interfaces/SeatService.java
@@ -1,4 +1,7 @@
 package com.flightbookings.flight_bookings.services.interfaces;
 
+import com.flightbookings.flight_bookings.models.Flight;
+
 public interface SeatService {
+    void initializeSeats(Flight flight, int numRows);
 }
