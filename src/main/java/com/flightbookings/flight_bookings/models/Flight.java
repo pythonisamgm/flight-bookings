b82cf2b42350c7d2df98a0b8c4ed59659fe9086b
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-24 10:21:31 +0200

    feat: add attributes, constructors, getters and setters for Flight entity in order to implement Seat logic

--- src/main/java/com/flightbookings/flight_bookings/models/Flight.java
+++ src/main/java/com/flightbookings/flight_bookings/models/Flight.java
@@ -1,4 +1,52 @@
 package com.flightbookings.flight_bookings.models;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="flight")
 public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long flightId;
+
+    private int numRows;
+
+    @OneToMany(mappedBy = "flight", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Seat> seats = new ArrayList<>();
+
+    public Flight() {
+    }
+
+    public Flight(Long flightId, int numRows, List<Seat> seats) {
+        this.flightId = flightId;
+        this.numRows = numRows;
+        this.seats = seats;
+    }
+
+    public Long getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(Long flightId) {
+        this.flightId = flightId;
+    }
+
+    public int getNumRows() {
+        return numRows;
+    }
+
+    public void setNumRows(int numRows) {
+        this.numRows = numRows;
+    }
+
+    public List<Seat> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(List<Seat> seats) {
+        this.seats = seats;
+    }
 }
