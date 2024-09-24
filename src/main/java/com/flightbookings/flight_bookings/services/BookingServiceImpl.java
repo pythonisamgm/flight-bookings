fcb4e9967d33afc1208f45d114c16fa7b5e50e4b
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-24 13:37:40 +0200

    feat: add method to create and book a seat in flight

--- src/main/java/com/flightbookings/flight_bookings/services/BookingServiceImpl.java
+++ src/main/java/com/flightbookings/flight_bookings/services/BookingServiceImpl.java
@@ -1,21 +1,52 @@
 package com.flightbookings.flight_bookings.services;
 
+import com.flightbookings.flight_bookings.exceptions.FlightNotFoundException;
+import com.flightbookings.flight_bookings.exceptions.PassengerNotFoundException;
+import com.flightbookings.flight_bookings.models.Booking;
+import com.flightbookings.flight_bookings.models.Flight;
+import com.flightbookings.flight_bookings.models.Passenger;
+import com.flightbookings.flight_bookings.models.Seat;
 import com.flightbookings.flight_bookings.repositories.IBookingRepository;
 import com.flightbookings.flight_bookings.repositories.IFlightRepository;
+import com.flightbookings.flight_bookings.repositories.IPassengerRepository;
 import com.flightbookings.flight_bookings.repositories.ISeatRepository;
 import com.flightbookings.flight_bookings.services.interfaces.BookingService;
+import com.flightbookings.flight_bookings.services.interfaces.SeatService;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class BookingServiceImpl implements BookingService {
 
     private final IBookingRepository bookingRepository;
     private final ISeatRepository seatRepository;
+    private final IFlightRepository flightRepository;
+    private final IPassengerRepository passengerRepository;
+    private final SeatService seatService;
 
-    public BookingServiceImpl(IBookingRepository bookingRepository, ISeatRepository seatRepository) {
+    public BookingServiceImpl(IBookingRepository bookingRepository, ISeatRepository seatRepository, IFlightRepository flightRepository, IPassengerRepository passengerRepository, SeatService seatService) {
         this.bookingRepository = bookingRepository;
         this.seatRepository = seatRepository;
+        this.flightRepository = flightRepository;
+        this.passengerRepository = passengerRepository;
+        this.seatService = seatService;
     }
+    public Booking createBooking(Long flightId, Long passengerId, String seatName) {
+        Flight flight = flightRepository.findById(flightId)
+                .orElseThrow(() -> new FlightNotFoundException("Flight not found"));
 
+        Passenger passenger = passengerRepository.findById(passengerId)
+                .orElseThrow(() -> new PassengerNotFoundException("Passenger not found"));
+
+        Seat seat = seatService.reserveSeat(flight, seatName);
+
+        Booking booking = new Booking( null, LocalDateTime.now(),passenger, flight,  seat);
+        seat.setBooking(booking);
+
+        bookingRepository.save(booking);
+
+        return booking;
+    }
 
 }
