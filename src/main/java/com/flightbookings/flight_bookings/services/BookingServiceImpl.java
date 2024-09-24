f3002ba51e65faa9e5eb3953afb165d2808e7e73
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-24 10:55:19 +0200

    feat: add constructors in services' files

--- src/main/java/com/flightbookings/flight_bookings/services/BookingServiceImpl.java
+++ src/main/java/com/flightbookings/flight_bookings/services/BookingServiceImpl.java
@@ -1,8 +1,17 @@
 package com.flightbookings.flight_bookings.services;
 
+import com.flightbookings.flight_bookings.repositories.IBookingRepository;
+import com.flightbookings.flight_bookings.repositories.IFlightRepository;
 import com.flightbookings.flight_bookings.services.interfaces.BookingService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookingServiceImpl implements BookingService {
+
+    private final IBookingRepository bookingRepository;
+
+    public BookingServiceImpl(IBookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
+
 }
--- src/main/java/com/flightbookings/flight_bookings/services/FlightServiceImpl.java
+++ src/main/java/com/flightbookings/flight_bookings/services/FlightServiceImpl.java
@@ -1,8 +1,16 @@
 package com.flightbookings.flight_bookings.services;
 
+import com.flightbookings.flight_bookings.repositories.IFlightRepository;
 import com.flightbookings.flight_bookings.services.interfaces.FlightService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FlightServiceImpl implements FlightService {
+
+    private final IFlightRepository flightRepository;
+
+    public FlightServiceImpl(IFlightRepository flightRepository) {
+        this.flightRepository = flightRepository;
+    }
+
 }
