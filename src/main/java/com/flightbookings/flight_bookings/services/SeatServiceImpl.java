f8c294623c5febf6da8f6e67e896a5328b4154e4
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-24 11:59:02 +0200

    feat: add functionality to create seats when a flight is initialized

--- src/main/java/com/flightbookings/flight_bookings/services/SeatServiceImpl.java
+++ src/main/java/com/flightbookings/flight_bookings/services/SeatServiceImpl.java
@@ -1,8 +1,14 @@
 package com.flightbookings.flight_bookings.services;
 
+import com.flightbookings.flight_bookings.models.ESeatLetter;
+import com.flightbookings.flight_bookings.models.Flight;
+import com.flightbookings.flight_bookings.models.Seat;
 import com.flightbookings.flight_bookings.repositories.ISeatRepository;
 import com.flightbookings.flight_bookings.services.interfaces.SeatService;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class SeatServiceImpl implements SeatService {
@@ -11,5 +17,18 @@ public class SeatServiceImpl implements SeatService {
 
     public SeatServiceImpl(ISeatRepository seatRepository) {
         this.seatRepository = seatRepository;
+    }
+    @Override
+    public void initializeSeats(Flight flight, int numRows) {
+        List<Seat> seats = new ArrayList<>();
+
+        for (int row = 1; row <= numRows; row++) {
+            for (ESeatLetter letter : ESeatLetter.values()) {
+                Seat seat = new Seat(null, row, letter, false, flight, null);
+                seats.add(seat);
+            }
+        }
+        seatRepository.saveAll(seats);
+        flight.setSeats(seats);
     }
 }
