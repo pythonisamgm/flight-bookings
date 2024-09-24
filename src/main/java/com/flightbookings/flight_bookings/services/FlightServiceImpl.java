610072a36160257339c42423d530517625a755ae
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-24 10:21:55 +0200

    feat: add notation service to serviceImpl files

--- src/main/java/com/flightbookings/flight_bookings/services/FlightServiceImpl.java
+++ src/main/java/com/flightbookings/flight_bookings/services/FlightServiceImpl.java
@@ -1,4 +1,13 @@
 package com.flightbookings.flight_bookings.services;
 
+import com.flightbookings.flight_bookings.repositories.IFlightRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FlightServiceImpl {
+
+    private final IFlightRepository flightRepository;
+
+    public FlightServiceImpl(IFlightRepository flightRepository){this.flightRepository = flightRepository;}
+
 }
