d586ef26db01a0b3dce9db4b23eaf266e93bcb4d
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-24 15:33:28 +0200

    add exception

--- src/main/java/com/flightbookings/flight_bookings/exceptions/NoSeatAssignedException.java
+++ src/main/java/com/flightbookings/flight_bookings/exceptions/NoSeatAssignedException.java
@@ -1,2 +1,12 @@
-package com.flightbookings.flight_bookings.exceptions;public class NoSeatAssignedException {
+package com.flightbookings.flight_bookings.exceptions;
+
+public class NoSeatAssignedException extends RuntimeException {
+
+    public NoSeatAssignedException(String message) {
+        super(message);
+    }
+
+    public NoSeatAssignedException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
