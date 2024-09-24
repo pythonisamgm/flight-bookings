5956a6dd176c14102e2bc9cb00fee0d66b4206ad
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-24 13:22:45 +0200

    refactor: modify logic so it responds to the single responsibility principle

--- src/main/java/com/flightbookings/flight_bookings/repositories/ISeatRepository.java
+++ src/main/java/com/flightbookings/flight_bookings/repositories/ISeatRepository.java
@@ -1,11 +1,11 @@
 package com.flightbookings.flight_bookings.repositories;
 
-import com.flightbookings.flight_bookings.models.ESeatLetter;
+import com.flightbookings.flight_bookings.models.Flight;
 import com.flightbookings.flight_bookings.models.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ISeatRepository extends JpaRepository<Seat, Long> {
-    Optional<Seat> fetchSeatData(Long flightId, int row, ESeatLetter seatLetter);
+    Optional<Seat> findBySeatReference(Flight flight, String seatName);
 }
