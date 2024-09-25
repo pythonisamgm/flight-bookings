d703fe35224cf1daf96a4cd5cc330b9108af820a
JuanMolinaRey <Juanmolinarey7@gmail.com>
2024-09-25 14:33:14 +0200

    update_delete_methods_Aiport_service

--- src/main/java/com/flightbookings/flight_bookings/services/BookingServiceImpl.java
+++ src/main/java/com/flightbookings/flight_bookings/services/BookingServiceImpl.java
@@ -1,7 +1,6 @@
 package com.flightbookings.flight_bookings.services;
 
 import com.flightbookings.flight_bookings.repositories.IBookingRepository;
-import com.flightbookings.flight_bookings.repositories.IFlightRepository;
 import com.flightbookings.flight_bookings.services.interfaces.BookingService;
 import org.springframework.stereotype.Service;
 
