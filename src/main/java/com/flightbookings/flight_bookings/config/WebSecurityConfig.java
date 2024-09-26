eab6c1d7471cdd7d2d2f5bc1dbe5bb03524f70d5
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-26 15:29:41 +0200

    feat: add endpoints

--- src/main/java/com/flightbookings/flight_bookings/config/WebSecurityConfig.java
+++ src/main/java/com/flightbookings/flight_bookings/config/WebSecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -30,10 +32,20 @@ public class WebSecurityConfig {
                         csrf.disable())
                 .authorizeHttpRequests(authRequest ->
                         authRequest
-                                .requestMatchers("/api/auth/**").permitAll()
-                                .requestMatchers("/api/auth/register").permitAll()
-                                .requestMatchers("/api/auth/login").permitAll()
+                                .requestMatchers("/api/v1/auth/**").permitAll()
+                                .requestMatchers("/api/v1/auth/register").permitAll()
+                                .requestMatchers("/api/v1/auth/login").permitAll()
                                 .requestMatchers("/getflight").permitAll()
+                                .requestMatchers("/api/v1/passengers/create").permitAll()
+                                .requestMatchers("/api/v1/passengers/{id}").permitAll()
+                                .requestMatchers("/api/v1/passengers").permitAll()
+                                .requestMatchers("/api/v1/passengers/update/{id}").permitAll()
+                                .requestMatchers("/api/v1/passengers/delete/{id}").permitAll()
+                                .requestMatchers("/api/v1/flight/create").permitAll()
+                                .requestMatchers("/api/v1/flight/{id}").permitAll()
+                                .requestMatchers("/api/v1/flight").permitAll()
+                                .requestMatchers("/api/v1/flight/update/{id}").permitAll()
+                                .requestMatchers("/api/v1/flight/delete/{id}").permitAll()
 
                                 .anyRequest().authenticated()
                 )
