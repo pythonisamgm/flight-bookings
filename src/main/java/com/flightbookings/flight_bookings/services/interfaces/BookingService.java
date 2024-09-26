39a8c632fd2dc43b955e604dcaf6fa14500a5351
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-26 09:37:05 +0200

    feat: implement methods in service file

--- src/main/java/com/flightbookings/flight_bookings/services/interfaces/BookingService.java
+++ src/main/java/com/flightbookings/flight_bookings/services/interfaces/BookingService.java
@@ -2,9 +2,21 @@ package com.flightbookings.flight_bookings.services.interfaces;
 
 import com.flightbookings.flight_bookings.models.Booking;
 
+import java.util.List;
+
 public interface BookingService {
 
     Booking createBooking(Long flightId, Long passengerId, String seatName);
 
     Booking updateBooking(Booking updatedBooking);
+
+    Booking createBooking2(Booking booking);
+
+    Booking getBookingById(Long id);
+
+    List<Booking> getAllBookings();
+
+    Booking updateBooking2(Long id, Booking bookingDetails);
+
+    boolean deleteBooking(Long id);
 }
--- src/main/java/com/flightbookings/flight_bookings/services/interfaces/FlightService.java
+++ src/main/java/com/flightbookings/flight_bookings/services/interfaces/FlightService.java
@@ -1,4 +1,20 @@
 package com.flightbookings.flight_bookings.services.interfaces;
 
+import com.flightbookings.flight_bookings.models.Flight;
+import com.flightbookings.flight_bookings.repositories.IFlightRepository;
+import com.flightbookings.flight_bookings.repositories.ISeatRepository;
+
+import java.util.List;
+
 public interface FlightService {
+
+    Flight createFlight(Flight flight);
+
+    Flight getFlightById(Long id);
+
+    List<Flight> getAllFlights();
+
+    Flight updateFlight(Long id, Flight flightDetails);
+
+    boolean deleteFlight(Long id);
 }
