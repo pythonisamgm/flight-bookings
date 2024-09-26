390baccebd272d3919fe325dcbe02ac4f5a44380
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-26 13:25:34 +0200

    commit

--- src/main/java/com/flightbookings/flight_bookings/config/WebSecurityConfig.java
+++ src/main/java/com/flightbookings/flight_bookings/config/WebSecurityConfig.java
@@ -30,8 +30,10 @@ public class WebSecurityConfig {
                         csrf.disable())
                 .authorizeHttpRequests(authRequest ->
                         authRequest
+                                .requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/api/auth/register").permitAll()
                                 .requestMatchers("/api/auth/login").permitAll()
-                                .requestMatchers("/api/auth/register").hasAuthority("ADMIN")
+                                .requestMatchers("/getflight").permitAll()
 
                                 .anyRequest().authenticated()
                 )
