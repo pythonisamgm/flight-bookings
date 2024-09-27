266a956d903f65d1fa46cfffbdbb7ab659e25e68
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-09-27 14:39:07 +0200

    update data.sql and added line 10 and 11 in application.porperties

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO flight (availability, capacity_plane, flight_airplane, flight_number, flight_price, num_rows, arrival_time, departure_time, flight_id)
+VALUES
+(b'1', '1', '1', '1', '1', '1', '2024-09-04 11:47:46.000000', '2024-09-05 11:47:46.000000', '1');
