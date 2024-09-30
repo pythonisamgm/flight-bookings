68226eee0280a09e5cf9019d6249ceff1c2bee5b
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-09-30 12:22:44 +0200

    decomment airport files to test workflow

--- src/main/java/com/flightbookings/flight_bookings/controllers/AirportController.java
+++ src/main/java/com/flightbookings/flight_bookings/controllers/AirportController.java
@@ -1,20 +1,46 @@
-//package com.flightbookings.flight_bookings.controllers;
-//
-//import com.flightbookings.flight_bookings.services.interfaces.AirportService;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@CrossOrigin("*")
-//@RestController
-//@RequestMapping("api/v1/airport")
-//public class AirportController {
-//
-//    private final AirportService airportService;
-//
-//    public AirportController(AirportService airportService) {
-//        this.airportService = airportService;
-//    }
-//
-//
-//}
+package com.flightbookings.flight_bookings.controllers;
+
+import com.flightbookings.flight_bookings.models.Airport;
+import com.flightbookings.flight_bookings.services.interfaces.AirportService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("api/v1/airport")
+public class AirportController {
+
+    private final AirportService airportService;
+
+    public AirportController(AirportService airportService) {
+        this.airportService = airportService;
+    }
+    @PostMapping
+    public ResponseEntity<Airport> createAirport(@RequestBody Airport airport) {
+        Airport newAirport = airportService.createAirport(airport);
+        return ResponseEntity.ok(newAirport);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Airport> getAirportById(@PathVariable Long id) {
+        Optional<Airport> airport = airportService.getAirportById(id);
+        return airport.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Airport>> getAllAirports() {
+        List<Airport> airports = airportService.getAllAirports();
+        return ResponseEntity.ok(airports);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Airport> updateAirport(@RequestBody Airport airport, @PathVariable Long id) {
+        Airport updatedAirport = airportService.updateAirport(airport, id);
+        return ResponseEntity.ok(updatedAirport);
+    }
+
+}
