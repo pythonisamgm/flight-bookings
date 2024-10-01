b134efeb56950009d84c4b5c5465b5ee9fa5bff7
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-10-01 09:34:53 +0200

    added WebSecurityConfig endpoints

--- src/main/java/com/flightbookings/flight_bookings/config/WebSecurityConfig.java
+++ src/main/java/com/flightbookings/flight_bookings/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ package com.flightbookings.flight_bookings.config;
 import com.flightbookings.flight_bookings.jwt.AuthTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -36,17 +37,36 @@ public class WebSecurityConfig {
                                 .requestMatchers("/api/v1/auth/**").permitAll()
                                 .requestMatchers("/api/v1/auth/register").permitAll()
                                 .requestMatchers("/api/v1/auth/login").permitAll()
-                                .requestMatchers("/getflight").permitAll()
+
+                                .requestMatchers("/api/v1/bookings/create").permitAll()
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/bookings/{id}").permitAll()
+                                .requestMatchers("/api/v1/bookings/create2").permitAll()
+                                .requestMatchers(HttpMethod.GET,"/api/v1/bookings/{id}").permitAll()
+                                .requestMatchers("/api/v1/bookings/").permitAll()
+                                .requestMatchers("/api/v1/bookings/update/{id}").permitAll()
+                                .requestMatchers("/api/v1/bookings/delete/{id}").permitAll()
+
+                                .requestMatchers("api/v1/flight/create").permitAll()
+                                .requestMatchers("api/v1/flight/{id}").permitAll()
+                                .requestMatchers("api/v1/flight/").permitAll()
+                                .requestMatchers("api/v1/flight/update/{id}").permitAll()
+                                .requestMatchers("api/v1/flight/delete/{id}").permitAll()
+                                .requestMatchers("api/v1/flight/{id}/cancel").permitAll()
+                                .requestMatchers("api/v1/flight/{id}/delay").permitAll()
+                                .requestMatchers("api/v1/flight/updateAvailability").permitAll()
+                                .requestMatchers("api/v1/flight/byAirplaneType").permitAll()
+
                                 .requestMatchers("/api/v1/passengers/create").permitAll()
                                 .requestMatchers("/api/v1/passengers/{id}").permitAll()
                                 .requestMatchers("/api/v1/passengers").permitAll()
                                 .requestMatchers("/api/v1/passengers/update/{id}").permitAll()
                                 .requestMatchers("/api/v1/passengers/delete/{id}").permitAll()
-                                .requestMatchers("/api/v1/flight/create").permitAll()
-                                .requestMatchers("/api/v1/flight/{id}").permitAll()
-                                .requestMatchers("/api/v1/flight/").permitAll()
-                                .requestMatchers("/api/v1/flight/update/{id}").permitAll()
-                                .requestMatchers("/api/v1/flight/delete/{id}").permitAll()
+
+                                .requestMatchers("api/v1/user/create").permitAll()
+                                .requestMatchers("api/v1/user/{id}").permitAll()
+                                .requestMatchers("api/v1/user/").permitAll()
+                                .requestMatchers("api/v1/user/update/{id}").permitAll()
+                                .requestMatchers("api/v1/user/delete/{id}").permitAll()
 
                                 .anyRequest().authenticated()
                 )
