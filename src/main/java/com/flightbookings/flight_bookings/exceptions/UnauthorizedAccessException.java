80a714c2b1b2e061288daeeaa1f6783c21b54522
pythonisamgm <pythonisamgm@users.noreply.github.com>
2024-10-01 11:25:54 +0000

    deploy: c8c4ce1327b44ab7352fef49436901de18873bea

--- src/main/java/com/flightbookings/flight_bookings/exceptions/UnauthorizedAccessException.java
+++
@@ -1,7 +0,0 @@
-package com.flightbookings.flight_bookings.exceptions;
-
-public class UnauthorizedAccessException extends RuntimeException {
-    public UnauthorizedAccessException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/flightbookings/flight_bookings/repositories/IFlightRepository.java
+++
@@ -1,7 +0,0 @@
-package com.flightbookings.flight_bookings.repositories;
-
-import com.flightbookings.flight_bookings.models.Flight;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IFlightRepository extends JpaRepository<Flight, Long> {
-}
