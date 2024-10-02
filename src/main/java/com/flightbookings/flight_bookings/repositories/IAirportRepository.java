0fe2325577e13b3e998bf023ce8c54710ffc89a7
pythonisamgm <pythonisamgm@users.noreply.github.com>
2024-10-02 12:23:54 +0000

    deploy: 7614740de8c75d1d0a91ce5729bb4b0d9f75c040

--- src/main/java/com/flightbookings/flight_bookings/repositories/IAirportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.flightbookings.flight_bookings.repositories;
-
-import com.flightbookings.flight_bookings.models.Airport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IAirportRepository extends JpaRepository<Airport, Long> {
-}
