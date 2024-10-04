9702efeb63bc05e887017b7ac17ff066e783837e
JuanMolinaRey <Juanmolinarey7@gmail.com>
2024-10-04 11:59:32 +0200

    update_dto

--- src/main/java/com/flightbookings/flight_bookings/services/interfaces/FlightDurationService.java
+++
@@ -1,9 +0,0 @@
-package com.flightbookings.flight_bookings.services.interfaces;
-
-import com.flightbookings.flight_bookings.models.Flight;
-
-import java.time.Duration;
-
-public interface FlightDurationService {
-    Duration calculateFlightDuration(Flight flight);
-}
