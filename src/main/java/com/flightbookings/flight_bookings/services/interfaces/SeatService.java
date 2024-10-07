661a430136bae1450fa16767c23450fd42d98105
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-10-07 08:45:59 +0200

    fix: fix seat service

--- src/main/java/com/flightbookings/flight_bookings/services/interfaces/SeatService.java
+++ src/main/java/com/flightbookings/flight_bookings/services/interfaces/SeatService.java
@@ -45,5 +45,5 @@ public interface SeatService {
      * @return the reserved Seat object.
      */
     @Transactional
-    SeatDTO reserveSeat(Flight flight, String seatName);
+    Seat reserveSeat(Flight flight, String seatName);
 }
