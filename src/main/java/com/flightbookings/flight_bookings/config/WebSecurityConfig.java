0398b2abfda29a4f506d22d66f070a5f3845098f
FJRJ3D <FJRJ3D@users.noreply.github.com>
2024-10-07 07:10:05 +0000

    deploy: 0322476706b508468a8b3e02df8af043d7eb7d4b

--- src/main/java/com/flightbookings/flight_bookings/config/WebSecurityConfig.java
+++ src/main/java/com/flightbookings/flight_bookings/config/WebSecurityConfig.java
@@ -51,8 +51,9 @@ public class WebSecurityConfig {
                                 .requestMatchers("/api/v1/auth/register").permitAll()
                                 .requestMatchers("/api/v1/auth/login").permitAll()
 
-                                .requestMatchers("/api/v1/bookings/create").hasAuthority("USER")
+                                .requestMatchers("/api/v1/bookings/create/{flightId}/{passengerId}/{seatName}/{userId}").hasAuthority("USER")
                                 .requestMatchers(HttpMethod.PUT,"/api/v1/bookings/{id}").hasAuthority("ADMIN")
+                                .requestMatchers(HttpMethod.PUT,"/api/v1/bookings/all").hasAuthority("ADMIN")
                                 //.requestMatchers("/api/v1/bookings/create2").permitAll()
                                 .requestMatchers(HttpMethod.GET,"/api/v1/bookings/{id}").hasAnyAuthority("ADMIN", "USER")
                                 .requestMatchers("/api/v1/bookings/").hasAnyAuthority("ADMIN", "USER")
@@ -81,10 +82,7 @@ public class WebSecurityConfig {
                                 .requestMatchers("api/v1/user/update/{id}").hasAuthority("ADMIN")
                                 .requestMatchers("api/v1/user/delete/{id}").hasAuthority("ADMIN")
 
-                                .requestMatchers(HttpMethod.POST,"api/v1/airport").hasAuthority("ADMIN")
-                                .requestMatchers(HttpMethod.GET,"api/v1/airport").hasAuthority("ADMIN")
-                                .requestMatchers(HttpMethod.GET,"api/v1/airport/{id}").hasAuthority("ADMIN")
-                                .requestMatchers(HttpMethod.PUT,"api/v1/airport/{id}").hasAuthority("ADMIN")
+                                .requestMatchers("api/airports").hasAuthority("ADMIN")
 
                                 .anyRequest().authenticated()
                 )
