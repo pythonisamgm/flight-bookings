323cd2d71a7fe62a3d77ba211c4bd8421fe257a1
pythonisamgm <pythonisamgm@users.noreply.github.com>
2024-10-08 06:41:23 +0000

    deploy: 34bd371acb13b6c925eca8c28b3cb7083ba8f856

--- src/main/java/com/flightbookings/flight_bookings/services/FlightDurationServiceImpl.java
+++ src/main/java/com/flightbookings/flight_bookings/services/FlightDurationServiceImpl.java
@@ -5,9 +5,19 @@ import com.flightbookings.flight_bookings.services.interfaces.FlightDurationServ
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
-
+/**
+ * Service implementation for calculating the duration of a flight.
+ * This service calculates the time difference between a flight's departure and arrival times.
+ */
 @Service
 public class FlightDurationServiceImpl implements FlightDurationService {
+    /**
+     * Calculates the duration of a given flight based on its departure and arrival times.
+     *
+     * @param flight the Flight object containing departure and arrival times.
+     * @return a Duration object representing the time difference between departure and arrival,
+     *         or null if either the departure or arrival time is not set.
+     */
     @Override
     public Duration calculateFlightDuration(Flight flight) {
         if (flight.getDepartureTime() != null && flight.getArrivalTime() != null) {
