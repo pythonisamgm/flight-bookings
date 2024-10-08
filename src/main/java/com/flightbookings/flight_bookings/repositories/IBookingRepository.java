48b51e2031c99874178219f78d0ee632ab6282fc
pythonisamgm <pythonisamgm@users.noreply.github.com>
2024-10-08 06:32:20 +0000

    deploy: 793dea0674208b30f16941b5f9ace8ee501d52d6

--- src/main/java/com/flightbookings/flight_bookings/repositories/IBookingRepository.java
+++ src/main/java/com/flightbookings/flight_bookings/repositories/IBookingRepository.java
@@ -5,7 +5,16 @@ import com.flightbookings.flight_bookings.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-
+/**
+ * Repository interface for managing {@link Booking} entities.
+ * Provides CRUD operations and a method to find bookings by user.
+ */
 public interface IBookingRepository extends JpaRepository<Booking, Long> {
+    /**
+     * Finds a list of bookings associated with a given user.
+     *
+     * @param user the user whose bookings are to be found.
+     * @return a list of bookings for the specified user.
+     */
     List<Booking> findByUser(User user);
 }
