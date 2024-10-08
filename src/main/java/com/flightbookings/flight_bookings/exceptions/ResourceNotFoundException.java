8ee4bc6fc25dc01d41912076d971c28115ba6833
pythonisamgm <pythonisamgm@users.noreply.github.com>
2024-10-08 06:17:30 +0000

    deploy: fe6404be8e5f50952319b5c8a9c9a35fcbc5875a

--- src/main/java/com/flightbookings/flight_bookings/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/flightbookings/flight_bookings/exceptions/ResourceNotFoundException.java
@@ -1,6 +1,14 @@
 package com.flightbookings.flight_bookings.exceptions;
 
+/**
+ * Exception thrown when a resource cannot be found.
+ */
 public class ResourceNotFoundException extends RuntimeException {
+    /**
+     * Constructs a new ResourceNotFoundException with the specified detail message.
+     *
+     * @param message the detail message.
+     */
     public ResourceNotFoundException(String message) {
         super(message);
     }
--- src/main/java/com/flightbookings/flight_bookings/exceptions/UnauthorizedAccessException.java
+++ src/main/java/com/flightbookings/flight_bookings/exceptions/UnauthorizedAccessException.java
@@ -1,6 +1,14 @@
 package com.flightbookings.flight_bookings.exceptions;
 
+/**
+ * Exception thrown when an unauthorized access attempt is made.
+ */
 public class UnauthorizedAccessException extends RuntimeException {
+    /**
+     * Constructs a new UnauthorizedAccessException with the specified detail message.
+     *
+     * @param message the detail message.
+     */
     public UnauthorizedAccessException(String message) {
         super(message);
     }
