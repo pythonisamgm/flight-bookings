2597d8310f92a67411f62f4dcacb95082b5f0c69
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-11-12 12:43:27 +0100

    feat: add repository annotation2

--- src/main/java/com/flightbookings/flight_bookings/repositories/IBookingRepository.java
+++ src/main/java/com/flightbookings/flight_bookings/repositories/IBookingRepository.java
@@ -3,12 +3,14 @@ package com.flightbookings.flight_bookings.repositories;
 import com.flightbookings.flight_bookings.models.BookingEntity;
 import com.flightbookings.flight_bookings.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 /**
  * Repository interface for managing {@link BookingEntity} entities.
  * Provides CRUD operations and a method to find bookings by user.
  */
+@Repository
 public interface IBookingRepository extends JpaRepository<BookingEntity, Long> {
     /**
      * Finds a list of bookings associated with a given user.
--- src/main/java/com/flightbookings/flight_bookings/repositories/IPassengerRepository.java
+++ src/main/java/com/flightbookings/flight_bookings/repositories/IPassengerRepository.java
@@ -2,9 +2,12 @@ package com.flightbookings.flight_bookings.repositories;
 
 import com.flightbookings.flight_bookings.models.PassengerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 /**
  * Repository interface for managing {@link PassengerEntity} entities.
  * Provides basic CRUD operations through JpaRepository.
  */
+@Repository
 public interface IPassengerRepository extends JpaRepository<PassengerEntity, Long> {
 }
